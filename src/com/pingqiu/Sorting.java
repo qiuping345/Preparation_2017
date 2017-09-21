b2e2a858a43e955c37381af8e28a63f49fd33a16
qiuping345 <qiuping345@gmail.com>
2017-09-21 00:23:52 -0700

    add merge sort and bubble sort.

--- src/com/pingqiu/Sorting.java
+++ src/com/pingqiu/Sorting.java
@@ -13,6 +13,14 @@ public class Sorting {
 		sorter.insertionSort(test2);
 		PrintUtil.printArray(test2);
 
+		int[] test3 = new int[] {42, 9, 17, 54, 602, -3, 54, 999, -11};
+		sorter.bubbleSort(test3);
+		PrintUtil.printArray(test3);
+		
+		int[] test4 = new int[] {42, 9, 17, 54, 602, -3, 54, 999, -11};
+		sorter.mergeSort(test4);
+		PrintUtil.printArray(test4);
+		
 	}
 	
 	
@@ -28,6 +36,54 @@ public class Sorting {
 				arr[j + 1] = arr[j];
 			}
 			arr[j + 1] = key; // take care it's 'arr[j+1]', it's failed the inner for loop condition.
+		}
+	}
+	
+	public void mergeSort(int[] arr) {
+		mergeSort(arr, 0, arr.length);
+	}
+	
+	public void mergeSort(int[] arr, int start, int end) {
+		int mid = start / 2 + end / 2;
+		if (end - start < 2) {
+			return;
+		}
+		
+		int[] arrLeft = new int[mid - start];
+		int[] arrRight = new int[end - mid];
+		System.arraycopy(arr, start, arrLeft, 0, arrLeft.length);
+		System.arraycopy(arr, mid, arrRight, 0, arrRight.length);
+		
+		mergeSort(arrLeft, 0, arrLeft.length);
+		mergeSort(arrRight, 0, arrRight.length);
+		
+		int iArr = start;
+		int iArrLeft = 0;
+		int iArrRight = 0;
+		
+		while (iArr < end) {
+			if (iArrLeft < arrLeft.length && iArrRight < arrRight.length) {
+				arr[iArr++] = arrLeft[iArrLeft] < arrRight[iArrRight] ? arrLeft[iArrLeft++] : arrRight[iArrRight++];
+			} else if (iArrLeft < arrLeft.length) {
+				arr[iArr++] = arrLeft[iArrLeft++];
+			} else {
+				arr[iArr++] = arrRight[iArrRight++];
+			}
+		}	
+	}
+	
+	
+	public void bubbleSort(int[] arr) {
+		if (arr == null || arr.length < 2) {
+			return;
+		}
+		   
+		for (int i = arr.length - 1; i >= 0; i--) {
+			for (int j = 0; j < i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					Util.swap(arr, j, j + 1);
+				}
+			}
 		}
 	}
 
