d9fc8421090580d521a7005264f48ee4542aaffa
Ping Qiu <piqiu@microsoft.com>
2017-10-19 16:42:25 -0700

    add RB tree

---
+++ src/com/pingqiu/RBTree.java
@@ -0,0 +1,33 @@
+package com.pingqiu;
+
+public class RBTree {
+	
+	private RBTreeNode root;
+	
+	public static class RBTreeNode {
+		public RBTreeNode left, right, parent;
+		public boolean isBlack;
+		public int size;
+		public Comparable value;
+	}
+	
+	public boolean isEmpty() {
+		return root == null;
+	}
+	
+	public void leftRotate(RBTreeNode node) {
+		
+		
+	}
+
+	public void rightRotate(RBTreeNode node) {
+		
+		
+	}
+	
+	public void insert(RBTreeNode node) {
+		
+		
+	}
+
+}
